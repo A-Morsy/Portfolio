836d5772f8f07b608a50ba2bc14614af230202bb
AbdelrahmanAshraf9079 <abdelrahman9709@gmail.com>
2023-07-08 22:56:03 +0300

    About section fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,40 @@
-import { BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { lazy, Suspense } from "react";
-import { Navbar } from "./components";
+// import StarsCanvas from "./components/StarsCanvas";
 
-const HomePage = lazy(() => import('./components'));
-const AboutPage = lazy(() => import('./components'));
-const ContactPage = lazy(() => import('./components'));
-const ExperiencePage = lazy(() => import('./components'));
-const TechPage = lazy(() => import('./components'));
-const WorksPage = lazy(() => import('./components'));
-const StarsCanvas = lazy(() => import('./components'));
+const LazyAbout = lazy(() => import("./components/About"));
+const LazyContact = lazy(() => import("./components/Contact"));
+const LazyExperience = lazy(() => import("./components/Experience"));
+const LazyHero = lazy(() => import("./components/Hero"));
+const LazyNavbar = lazy(() => import("./components/Navbar"));
+const LazyTech = lazy(() => import("./components/Tech"));
+const LazyWorks = lazy(() => import("./components/Works"));
+const LazyStars = lazy(() => import("./components/canvas/Stars"));
 
 const App = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <BrowserRouter>
-        <div className="relative z-0 bg-primary">
-          <div className="bg-hero-pattern bg-cover-bg-no-repeat bg-center">
-            <Navbar />
-             
-                <Route path="/" exact component={HomePage} />
-                <Route path="/about" component={AboutPage} />
-                <Route path="/contact" component={ContactPage} />
-                <Route path="/experience" component={ExperiencePage} />
-                <Route path="/tech" component={TechPage} />
-                <Route path="/works" component={WorksPage} />
-              
-            </div>
-            <div className="relative z-0">
-              <StarsCanvas />
-            </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover-bg-no-repeat bg-center">
+          <Suspense fallback={<div>Loading...</div>}>
+            <LazyNavbar />
+            <LazyHero />
+          </Suspense>
         </div>
-      </BrowserRouter>
-    </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <LazyAbout />
+          <LazyExperience />
+          <LazyTech />
+          <LazyWorks />
+        </Suspense>
+        <div className="relative z-0">
+          <Suspense fallback={<div>Loading...</div>}>
+            <LazyContact />
+            <LazyStars />
+          </Suspense>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
@@ -6,11 +6,12 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 import { SectionWrapper } from "../hoc";
+
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className="xs:w-[250px] w-full mx-5">
+  <Tilt className="xs:w-[250px] w-full mx-5 flex-row max-w-[250px]">
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      className="w-full green-pink-gradient p-[1px] rounded-[20px]  shadow-card"
     >
       <div
         options={{
@@ -18,7 +19,7 @@ const ServiceCard = ({ index, title, icon }) => (
           scale: 1,
           speed: 450,
         }}
-        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px]  flex justify-evenly items-center flex-col"
       >
         <img
           src={icon}
@@ -37,19 +38,21 @@ const ServiceCard = ({ index, title, icon }) => (
 const About = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview</h2>
-      </motion.div>
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
-      >
-        I'm a detail-oriented software developer, I bring my experience and a
-        passion for solving complex problems using new technologies. With
-        expertise in React, Ruby on Rails, and PostgreSQL ,
-        Let's join forces to create innovative and impactful solutions together.
-      </motion.p>
+      <Suspense fallback={<div>Loading...</div>}>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>Introduction</p>
+          <h2 className={styles.sectionHeadText}>Overview</h2>
+        </motion.div>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          I'm a detail-oriented software developer, I bring my experience and a
+          passion for solving complex problems using new technologies. With
+          expertise in React, Ruby on Rails, and PostgreSQL ,
+          Let's join forces to create innovative and impactful solutions together.
+        </motion.p>
+      </Suspense>
 
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
